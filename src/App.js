44a1810d27fa0f62469522bb756c6283b44e7dfc
ViniciusSViana <visouzaviana@gmail.com>
2024-06-13 17:28:31 -0300

    finalização com laytou e outros operadores

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Input from './components/Input';
-import { Container, Content, Row, Column } from "./styles";
+import { Container, Content, Row, Header, overlay } from "./styles";
 import Button from './components/Button';
 import { useState } from 'react';
 
@@ -40,12 +40,63 @@ const App = () => {
 
   }
 
+  const handleMinusNumbers = () => {
+
+    if (firstNumber === '0') {
+      setfirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('-');
+    } else {
+      const sum = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation('')
+    }
+
+  }
+
+  const handleMultpleNumbers = () => {
+
+    if (firstNumber === '0') {
+      setfirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('x');
+    } else {
+      const sum = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation('')
+    }
+
+  }
+
+  const handleDivNumbers = () => {
+
+    if (firstNumber === '0') {
+      setfirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('/');
+    } else {
+      const sum = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation('')
+    }
+
+  }
+
   const handleEquals = () => {
 
     if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
       switch (operation) {
         case '+':
           handleSumNumbers();
+          break;
+          case '-':
+          handleMinusNumbers();
+          break;
+          case 'x':
+          handleMultpleNumbers();
+          break;
+          case '/':
+            handleDivNumbers();
           break;
         default:
           break;
@@ -57,25 +108,28 @@ const App = () => {
 
   return (
     <Container>
+      <Header>
+        <h1>Calculadora feita com React</h1>
+      </Header>
       <Content>
         <Input value={currentNumber} />
         <Row>
           <Button label="AC" onClick={handleOnClear} />
           <Button label="()" onClick={() => handleAddNumber('')} />
           <Button label="%" onClick={() => handleAddNumber('')} />
-          <Button label="/" onClick={() => handleAddNumber('')} />
+          <Button label="/" onClick={handleDivNumbers} />
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')} />
           <Button label="8" onClick={() => handleAddNumber('8')} />
           <Button label="9" onClick={() => handleAddNumber('9')} />
-          <Button label="x" onClick={() => handleAddNumber('')} />
+          <Button label="x" onClick={handleMultpleNumbers} />
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')} />
           <Button label="5" onClick={() => handleAddNumber('5')} />
           <Button label="6" onClick={() => handleAddNumber('6')} />
-          <Button label="-" onClick={() => handleAddNumber('')} />
+          <Button label="-" onClick={handleMinusNumbers} />
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')} />
@@ -85,7 +139,7 @@ const App = () => {
         </Row>
         <Row>
           <Button label="0" onClick={() => handleAddNumber('0')} />
-          <Button label="," onClick={() => handleAddNumber(',')} />
+          <Button label="," onClick={() => handleAddNumber('.')} />
           <Button label="bcksp" onClick={() => handleAddNumber('bcksp')} />
           <Button label="=" onClick={handleEquals} />
         </Row>
--- src/styles.js
+++ src/styles.js
@@ -1,22 +1,27 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-color: #CACACA;
-
-    display: flex;
-    align-items: center;
-    justify-content: center; 
+   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  height: 100vh;
+  background-image: url('https://wallpapercave.com/wp/wp4923981.jpg'); /* Substitua pelo caminho da sua imagem */
+  background-size: cover;
+  background-position: center;
+  position: relative; 
+  overflow: auto;
 `
 
 export const Content = styled.div`
-    background-color: #FFFFFF;
-    width: 50%;
-    min-height: 340px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-    border-radius: 10px;
-
+  background-color: rgba(255, 255, 255, 0.5); 
+  width: 60%;
+  min-height: 350px;
+  display: flex;
+  flex-direction: column;
+  padding: 2px; 
+  margin-bottom: 40px;
 `
 
 export const Row = styled.div`
@@ -34,3 +39,20 @@ export const Column = styled.div`
     align-items:center;
 
 `
+
+export const Header = styled.div`
+    width: 100%;
+    text-align: center;
+    margin-top: 100PX;
+    margin-bottom: auto; auto;
+    color: #FFFF
+
+`
+export const Overlay = styled.div`
+ 
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.8); /* Fundo branco semi-transparente */
+`;
